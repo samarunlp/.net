ccf74a6a029fc432e5aee6256f99c1a6396ccd28
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-04-11 16:09:56 -0300

    Create Auto.cs

---
+++ Teoria/teoria6/Auto.cs
@@ -0,0 +1,13 @@
+namespace Teoria6;
+
+class Auto: Automotor
+{
+    /* NO es necesario porque ya tengo uno con las mismas caracteristicas, utilizo el codigo
+   public string Marca = "";
+   public int Modelo;
+   public TipoAuto Tipo;
+   public void Imprimir()
+       => Console.WriteLine($"{Marca} {Modelo}");*/
+       
+  public TipoAuto Tipo;
+}
