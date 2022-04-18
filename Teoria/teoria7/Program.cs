38b8c1c617f45edcbbcc27dc5ea49ed8ed1753e8
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-04-18 15:47:06 -0300

    Create Program.cs

---
+++ Teoria/teoria7/Program.cs
@@ -0,0 +1,12 @@
+using Teoria7;
+
+object[] vector = new object[] {
+       new Moto("Zanella"),
+       new Empleado("Juan"),
+       new Moto("Gilera")
+   };
+
+foreach (object o in vector)
+{
+   
+}
