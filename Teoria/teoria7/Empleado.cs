a810b37f033414ee7b588adf26063f602e004821
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-04-18 16:52:35 -0300

    Update Empleado.cs

--- Teoria/teoria7/Empleado.cs
+++ Teoria/teoria7/Empleado.cs
@@ -1,1 +1,22 @@
+namespace Teoria7;
+
+class Empleado : Persona, IImprimible, IComparable
+{
+   //Si me paro sobre IComparable y aprieto 'ctrl+.' implementa la interfaz
+   public Empleado(string nombre)//agrega
+      => Nombre = nombre;
+   public void Imprimir()//agrega
+      => Console.WriteLine($"Soy el empleado {Nombre}");
+
+   public int CompareTo(object? obj)//agrega pero no pone 'CompareTo', ojo
+    {
+        int result = 0;
+        if(obj is Empleado)
+        {
+           string nombre = ((Empleado)obj).Nombre; //orden alfabetico de los nombres
+           result= this.Nombre.CompareTo(nombre);
+        }
+        return result;
+    }
+}
 
