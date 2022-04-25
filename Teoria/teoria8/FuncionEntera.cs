b9c838f74856f59d3f5ebaaac63caa64f9e73ecb
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-04-25 16:18:55 -0300

    Update FuncionEntera.cs

--- Teoria/teoria8/FuncionEntera.cs
+++ Teoria/teoria8/FuncionEntera.cs
@@ -1,1 +1,10 @@
-
+namespace Teoria8;
+delegate int FuncionEntera(int n);
+/*
+Se puede declarar de 2 formas el delegado 
+-f= SumaUno; 
+-f = new FuncionEntera(SumaUno); //lo hace solo el compilador al constructor
+                                 //los entornos de desarrollo suelen declararlo de esta manera, la forma mas explicita
+describe de que forma son los metodos que puede recibir (no se guarda el metodo, tiene una referencia que sabe
+ invocar el metodo)
+ */
