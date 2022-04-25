7ceed56750b219ccdba43f600031f01b1928ed9f
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-04-25 16:18:21 -0300

    Update Auxiliar.cs

--- Teoria/teoria8/Auxiliar.cs
+++ Teoria/teoria8/Auxiliar.cs
@@ -1,10 +1,38 @@
-namespace Teoria7;
+namespace Teoria8;
 class Auxiliar{
     public void Procesar()
     {
-        Console.WriteLine(SumaUno(10));
-        Console.WriteLine(SumaDos(10));
+       /* 
+        FuncionEntera f;
+        f = SumaUno;
+        Console.WriteLine(f(10));
+        f = SumaDos;
+        Console.WriteLine(f(10));
+        */
+        int [] v = new int [] {11,5,90};
+        Aplicar(v, SumaDos);
+        Imprimir(v);
+        Aplicar(v,SumaUno);
+        Imprimir(v);
     }
     int SumaUno(int n) => n+1;
     int SumaDos(int n) => n+2;
+
+    void Aplicar(int [] v, FuncionEntera f)
+    {
+        //utliza el for porque se esta modificando
+        for (int i = 0; i < v.Length; i ++)
+        {
+            v [i] = f(v[i]);
+        }
+    }
+
+    void Imprimir(int [] v)
+    {
+        foreach (int i in v)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();// la consola no imprime hasta que no encuentra un fin de linea (apretando la consola interna f5, con dotnet run funciona sin el console)
+    }
 }
