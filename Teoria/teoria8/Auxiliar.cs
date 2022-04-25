6dfac216678ecaec3c9ef0b9a2f29444111d3fb2
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-04-25 15:47:20 -0300

    Create Auxiliar.cs

---
+++ Teoria/teoria8/Auxiliar.cs
@@ -0,0 +1,10 @@
+namespace Teoria7;
+class Auxiliar{
+    public void Procesar()
+    {
+        Console.WriteLine(SumaUno(10));
+        Console.WriteLine(SumaDos(10));
+    }
+    int SumaUno(int n) => n+1;
+    int SumaDos(int n) => n+2;
+}
