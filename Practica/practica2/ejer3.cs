30f54f3954c45254b9d6712f0a84f4a121a6b3b4
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-04-29 18:28:12 -0300

    Create ejer3.cs

---
+++ Practica/practica2/ejer3.cs
@@ -0,0 +1,8 @@
+/*3. Analizar la siguiente porción de código para calcular la sumatoria de 1 a 10. ¿Cuál es el error?
+¿Qué hace realmente?*/
+int sum = 0;
+int i = 1;
+while (i <= 10);
+{
+sum += i++;
+}
