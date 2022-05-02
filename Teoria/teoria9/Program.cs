046fff0dfa3097a1bea4fb4bf732be90377fe5f1
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-02 16:39:47 -0300

    Update Program.cs

--- Teoria/teoria9/Program.cs
+++ Teoria/teoria9/Program.cs
@@ -2,6 +2,8 @@ class Program
 {
     static void Main (string [] args) //vale tener un main "static void Main()" sin argumentos
     {
+        //Primer ejemplo
+        
         /*Primera solucion
         int a =17;
         int b= 23;
@@ -28,8 +30,8 @@ class Program
         st1=(string)o1; st2= (string)o2;
         Console.WriteLine($"st1={st1} y st2={st2}");
         */
-/*Tercera solucion
-Al usarlo se pierden muchos chequeos y controles de seguridad; y realiza boxing por ej en b=17. No es buena solucion, la sobrecarga es "mejor"
+       /*Tercera solucion
+        Al usarlo se pierden muchos chequeos y controles de seguridad; y realiza boxing por ej en b=17. No es buena solucion, la sobrecarga es "mejor"
         dynamic a =17; 
         dynamic b= 23;
         Swap(ref a, ref b);
@@ -38,12 +40,18 @@ Al usarlo se pierden muchos chequeos y controles de seguridad; y realiza boxing 
         dynamic st2 = "mundo";
         Swap(ref st1, ref st2);
         Console.WriteLine($"st1={st1} y st2={st2}");
-    }
- */
+    }*/
+
 //Para mejorar esto se utilizan los metodos genericos
-
-
-
+        //Cuarta solucion-->Eficiente 
+        int a =17; 
+        int b= 23;
+        Swap<int>(ref a, ref b);//Swap<int>(ref a, ref b); SearchOption invoca Swap<int> deduce
+        Console.WriteLine($"a={a} y b={b}");
+        string st1= "hola";  
+        string st2 = "mundo";
+        Swap<string>(ref st1, ref st2);
+        Console.WriteLine($"st1={st1} y st2={st2}");
 /*sobrecarga del metodo swap
      static void Swap(ref int a, ref int b){
          int x=a;
@@ -68,4 +76,31 @@ Al usarlo se pierden muchos chequeos y controles de seguridad; y realiza boxing 
          i=j;
          j=aux;
      }
+        
+        
+     //Segundo ejemplo
+     int i = (int) Maximo(100,5);//unboxing
+     Console.WriteLine(i);
+     string st= (String)Maximo("hola", "mundo");
+     Console.WriteLine(st);
+     Console.WriteLine(Maximo('A','B'));
+
+     /*static IComparable Maximo(IComparable a, IComparable b)
+     {
+         if(a.CompareTo(b)>0){
+             return a;
+         }
+         return b;
+     }*/
+   //meotodo generico
+     static T Maximo<T>(T a, T b) where T:IComparable //se utliza esto para poder solucionar el problema de CompareTo
+     {
+         if(a.CompareTo(b)>0){//el compilador no sabe si esta definido el CompareTo en el tipo T
+             return a;
+         }
+         return b;
+     }
+    }
+}
+        
 }
