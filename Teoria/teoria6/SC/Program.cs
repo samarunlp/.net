b2478b9b10937ef4418d13504c380e70ec30b3f3
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-09 17:24:52 -0300

    Update Program.cs

--- Teoria/teoria6/SC/Program.cs
+++ Teoria/teoria6/SC/Program.cs
@@ -1,1 +1,8 @@
+using Teoria6;
 
+Auto a= new Auto(" Ford ", 2000, TipoAuto.Deportivo);
+Colectivo c=new Colectivo("Mercedes", 2010, 20);
+//a.Imprimir();
+//c.Imprimir(); 
+Console.WriteLine(a.Marca + " " + a.Modelo);
+Console.WriteLine(a.Marca + " " + a.Modelo);
