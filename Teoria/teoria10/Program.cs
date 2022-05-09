3b084cecbc485720bf29d6d2713d8d936574695e
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-09 16:04:55 -0300

    Update Program.cs

--- Teoria/teoria10/Program.cs
+++ Teoria/teoria10/Program.cs
@@ -1,1 +1,28 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+       // ImprimirA();
+  /*Queremos que las A se sincronice, no tener que esperar que se ejecute las A para que se ejecuten los
+    guiones, para eso agregamos la linea 8 y 9. Se hace de manera sincronica (concurrente), pero no 
+    se garantiza que se va a terminar porque uno puede agarrarlo mucho tiempo o va a ir más lento uno*/
+        Task t = new Task(ImprimirA);
+        t.Start();
+        for( int i = 1; i <= 1000; i++)
+        {
+            Console.Write("-");
+        } 
+        Console.WriteLine(" FIN");
+    }
+
+    static void ImprimirA()
+    {
+        for( int i = 1; i <= 1000; i++)
+        {
+            Console.Write("A");
+        } 
+        Console.WriteLine(" FIN");
+    }
+}
+
 
