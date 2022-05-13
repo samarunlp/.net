f679837b1270389b232be164500d7981c3a579ce
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-13 18:13:05 -0300

    Update Ejercicio21.cs

--- Practica/practica1/Ejercicio21.cs
+++ Practica/practica1/Ejercicio21.cs
@@ -4,11 +4,12 @@ Analizar el siguiente código. ¿Cuál es la salida por consola?
 int i = 1;
 if (--i == 0)
 {
-Console.WriteLine("cero");
+Console.WriteLine("cero"); //Imprime "cero"
 }
 if (i++ == 0)
 {
-Console.WriteLine("cero");
+Console.WriteLine("cero"); //Imprime "cero"
 }
-Console.WriteLine(i);
+Console.WriteLine(i); //Imprime i (1)
+
 */
