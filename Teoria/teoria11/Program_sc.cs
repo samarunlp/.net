c9294c4f4d6f6f25ea2fdd96bcfefb71c7ba752c
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-16 16:10:44 -0300

    Create Program_sc.cs

---
+++ Teoria/teoria11/Program_sc.cs
@@ -0,0 +1,16 @@
+List<string> lista = new List<string>(){"uno","dos","tres"};
+IEnumerable <string> secuencia = lista.Select(st => "(" + st.ToUpper() + ")");
+Mostrar(secuencia); 
+IEnumerable <int> secuencia2 = secuencia.Select(st=> st.Length);
+Mostrar(secuencia2); 
+IEnumerable <double> secuencia3 = secuencia2.Select(dou => dou / 2.0);
+Mostrar(secuencia3); 
+
+void Mostrar<T>(IEnumerable<T> secuencia)
+{
+    foreach(T elemento in secuencia)
+    {
+        Console.WriteLine(elemento + "");
+    }
+    Console.WriteLine();
+}
