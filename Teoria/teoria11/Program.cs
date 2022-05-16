0095bf8c4af1c364e226f76c886c4e48ddcc174e
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-16 16:50:43 -0300

    Update Program.cs

--- Teoria/teoria11/Program.cs
+++ Teoria/teoria11/Program.cs
@@ -22,10 +22,21 @@ void Mostrar<T>(IEnumerable<T> secuencia)
 }
 
 var personas = Persona.GetLista();
-personas.ForEach(p=> Console.WriteLine(p)); //el foreach es de la lista generica porque linq no tiene
-Console.WriteLine();
-personas.Where(p => p.Edad >= 18).ToList().ForEach(p=> Console.WriteLine(p)); //con el toList lo convierto en list<personas> y luego lista las personas mayores de edad
+//personas.ForEach(p=> Console.WriteLine(p)); //el foreach es de la lista generica porque linq no tiene
+//Console.WriteLine();
+//personas.Where(p => p.Edad >= 18).ToList().ForEach(p=> Console.WriteLine(p)); //con el toList lo convierto en list<personas> y luego lista las personas mayores de edad
 
 /*
 .Select ( p => new {Nombre = p.nombre, Condicion = p.edad <18> "Menor" :"Mayor"}) crear objetos de un tipo anonimo, declara el nombre y utliza la condicion (creador ternario) para sacaber si es mayor de edad*/
 
+//con grup puedo elegir el criterio de agrupacion
+//grup.Key gruardo el valor por el cual yo lo agrupe, por ej argentina, bolivia
+//grup.count me devuelve la cantidad de grupos que almaceno
+
+//si quiero ordenarlo por edad, primero deberia haberse agrupado por edad y luego miro cual es menor
+var agrupadas= personas.GroupBy(p => p.Nombre[0]).OrderByDescending(g => g.Key); //utliza la inicial e invierte los valores
+foreach (var grupo in agrupadas)
+{
+    Console.WriteLine($"Inicial: {grupo.Key}");
+    grupo.ToList().ForEach(p => Console.WriteLine (" - " + p));
+}
