6824d6791472d9152c7536b776fe21fd3ddd67ce
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-16 17:43:16 -0300

    Update Program.cs

--- Teoria/teoria11/Segunda/Program.cs
+++ Teoria/teoria11/Segunda/Program.cs
@@ -2,4 +2,5 @@ using Escuela;
 using (var context = new EscuelaContext())
 {
     context.Database.EnsureCreated(); //si no existe la base de datos, la crea de acuerdo al modelo
+    EscuelaInit.Inicializar(context);
 }
