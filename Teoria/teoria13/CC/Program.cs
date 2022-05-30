954505c96b23378eafb40860dfd97aca1d531afd
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-30 16:45:03 -0300

    Update Program.cs

--- Teoria/teoria13/CC/Program.cs
+++ Teoria/teoria13/CC/Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
 using CalculoSimple;
 //Calculador calc= new Calculador(); //depende de calculador
 //ILogger logger= new Logger();
-ILogger logger= new LoggerArchivo();
+
+/*ILogger logger= new LoggerArchivo();
 Calculador calc= new Calculador(logger); 
-calc.Calcular(3);
+calc.Calcular(3);*/
+
 //acoplamiento fuerte
 /*evitar alta cohesión, dividir las cosas y agrupar comportamientos(cosas cohesivas que tengan pocas
 dependencia*/
+
+var servicios = new ServiceCollection();
+//para que se lo de debe registrarlos a todos
+servicios.AddTransient<ICalculador, Calculador>();//devuelve uno nuevo, transitorio. Cuando te pida un Icalculador devolveme una instancia de Calculador
+servicios.AddTransient<ILogger, Logger> ();//ando te pida un ILogger devolveme una instancia de Logger
+//var proveedor= new ProveedorServicios();
+var proveedor= servicios.BuildServiceProvider();
+var calc = proveedor.GetService <ICalculador>();
+//ICalculador calc= proveedor.GetCalculador();
+//calc.Calcular(3);
+calc?.Calcular(3);
