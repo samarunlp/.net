4048f1f73bc315debebc83d0db56ee519d131e48
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-30 16:24:59 -0300

    Update LoggerArchivo.cs

--- Teoria/teoria13/SC/LoggerArchivo.cs
+++ Teoria/teoria13/SC/LoggerArchivo.cs
@@ -3,7 +3,7 @@ class LoggerArchivo : ILogger
 {
     public void Log(string mensaje)
     {
-        using var sw = new StreamWriter("registro.log", true); //escribe el mensaje en un registro log
-        sw.WriteLine ($"{DateTime.Now} {mensaje}");//escribe una fecha en el archivo
+        using var sw = new StreamWriter("registro.log", true); 
+        sw.WriteLine ($"{DateTime.Now} {mensaje}");
     }
 }
