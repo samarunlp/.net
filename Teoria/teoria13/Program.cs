ab04533cfc8176305239a3e0ac34dbfaeb894b15
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-05-30 16:05:29 -0300

    Create Program.cs

---
+++ Teoria/teoria13/Program.cs
@@ -0,0 +1,6 @@
+using CalculoSimple;
+Calculador calc= new Calculador(); //depende de calculador
+calc.Calcular(3);
+//acoplamiento fuerte
+/*evitar alta cohesión, dividir las cosas y agrupar comportamientos(cosas cohesivas que tengan pocas
+dependencia*/
