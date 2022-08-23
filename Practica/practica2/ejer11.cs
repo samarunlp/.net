91ebf1fc6d9fd61a3234ad00e22e9ac322489cbe
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-23 18:48:07 -0300

    Create ejer11.cs

---
+++ Practica/practica2/ejer11.cs
@@ -0,0 +1,4 @@
+/*
+11. ¿Para qué sirve el método Split de la clase string? Usarlo para escribir en la consola todas
+las palabras (una por línea) de una frase ingresada por consola por el usuario.
+*/
