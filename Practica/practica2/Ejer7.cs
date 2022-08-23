10a1862009306868f9a5c5c3c59c78282c6dc857
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-23 18:44:43 -0300

    Create Ejer7.cs

---
+++ Practica/practica2/Ejer7.cs
@@ -0,0 +1,17 @@
+/*
+7.Analizar el siguiente código. ¿Qué líneas producen error de compilación y por qué?
+char c;
+char? c2;
+string? st;
+c = "";
+c = '';
+c = null;
+c2 = null;
+c2 = (65 as char?);
+st = "";
+st = '';
+st = null;
+st = (char)65;
+st = (string)65;
+st = 47.89.ToString();
+*/
