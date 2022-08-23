41e89304b6c8b43b51a591a5ec089d1de46cd42c
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-23 18:39:11 -0300

    Create ejer5.cs

---
+++ Practica/practica2/ejer5.cs
@@ -0,0 +1,7 @@
+/*
+¿Qué hace la instrucción
+int[]? vector = new int[0];
+¿asigna a la variable vector el valor null?
+*/
+//Declara, instancia e inicializa un vector con 1 elemento entero(int)
+//si porque el signo esta despues de los corchetes. 
