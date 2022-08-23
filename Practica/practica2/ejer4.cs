5c8da5ce165e0b2127e8d5d22a993e6b70ff04cb
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-23 18:34:44 -0300

    Create ejer4.cs

---
+++ Practica/practica2/ejer4.cs
@@ -0,0 +1,9 @@
+/*
+¿Cuál es la salida por consola si no se pasan argumentos por la línea de comandos?
+Console.WriteLine(args == null);
+Console.WriteLine(args.Length);
+*/
+/*Imprime
+False porque 
+0 porque no contiene ningun valor
+*/
