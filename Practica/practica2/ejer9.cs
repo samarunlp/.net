76719ba17566aa3988ddca46713e7b7096629f5e
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-23 18:47:26 -0300

    Create ejer9.cs

---
+++ Practica/practica2/ejer9.cs
@@ -0,0 +1,6 @@
+/*
+9. Investigar acerca de la clase StringBuilder del espacio de nombre System.Text ¿En qué
+circunstancias es preferible utilizar StringBuilder en lugar de utilizar string? Implementar
+un caso de ejemplo en el que el rendimiento sea claramente superior utilizando
+StringBuilder en lugar de string y otro en el que no.
+*/
