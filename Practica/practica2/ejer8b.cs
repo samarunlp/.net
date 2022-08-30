d29dd1e37071bf013c6821c05fd0c9e2f8417ba0
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-30 17:35:10 -0300

    Update ejer8b.cs

--- Practica/practica2/ejer8b.cs
+++ Practica/practica2/ejer8b.cs
@@ -1,3 +1,11 @@
 /*8. Escribir un programa que reciba una lista de nombres como parámetro e imprima por consola un
 saludo personalizado para cada uno de ellos.
 b) utilizando la sentencia foreach*/
+
+List<String> lista = new List<String> () {"Samar","Emanuel","Rocío"};
+
+foreach (var i in lista)
+{
+    Console.WriteLine("Hola "+lista[]);
+}
+//verrr
