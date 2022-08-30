0b0e897b80ff0fa3ca5865e68a5ef60b2c4d2106
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-30 17:19:53 -0300

    Update ejer7.cs

--- Practica/practica2/ejer7.cs
+++ Practica/practica2/ejer7.cs
@@ -3,15 +3,15 @@
 char c;
 char? c2;
 string? st;
-c = "";
-c = '';
-c = null;
-c2 = null;
-c2 = (65 as char?);
+c = ""; //quiere asignarle un string a un tipo char
+c = ''; //
+c = null; //es no nulable, por defecto tiene valores indefinidos no se le puede asignar null
+c2 = null; /es no nulable, por defecto tiene valores indefinidos no se le puede asignar null por mas que tenga el signo
+c2 = (65 as char?); //tira un warning porque char sera null
 st = "";
-st = '';
-st = null;
-st = (char)65;
-st = (string)65;
-st = 47.89.ToString();
+st = ''; //para asignarle a un string "vacio" se hace con doble comillas
+st = null; //tirara error porque antes se le puse el signo ?
+st = (char)65; //no se puede convertir un char a string de esa manera
+st = (string)65; no se puede convertir un intero a string de esa manera
+st = 47.89.ToString(); 
 */
