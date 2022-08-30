5fcbd7c1526d8a86d86563814b8d7f0eb12dc752
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-30 17:44:03 -0300

    Create ejer19.cs

---
+++ Practica/practica2/ejer19.cs
@@ -0,0 +1,19 @@
+/*
+19. Codificar el método Imprimir para que el siguiente código produzca la salida por consola que
+se observa. Considerar que el usuario del método Imprimir podría querer más adelante
+imprimir otros datos, posiblemente de otros tipos pasando una cantidad distinta de parámetros
+cada vez que invoque el método.
+Tip: usar params
+
+
+
+Imprimir(1, "casa", 'A', 3.4, DayOfWeek.Saturday);
+Imprimir(1, 2, "tres");
+Imprimir();
+Imprimir("-------------");
+
+/*Lo que muestra en consola:
+ 1 casa A 3,4 Saturday
+ 1 2 tres
+*/
+*/
