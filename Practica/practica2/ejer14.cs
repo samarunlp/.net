4802624448c751cd6b518e538e76f0c4055bde87
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-30 17:41:03 -0300

    Create ejer14.cs

---
+++ Practica/practica2/ejer14.cs
@@ -0,0 +1,7 @@
+/*
+Implementar un programa que muestre todos los números primos entre 1 y un número natural
+dado (pasado al programa como argumento por la línea de comandos). Definir el método bool
+EsPrimo(int n) que devuelve true sólo si n es primo. Esta función debe comprobar si n es
+divisible por algún número entero entre 2 y la raíz cuadrada de n. (Nota: Math.Sqrt(d)
+devuelve la raíz cuadrada de d)
+*/
