dbdf47a23823c7a0a3a1a9c012832536a02e7b4b
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-30 17:41:55 -0300

    Create ejer16.cs

---
+++ Practica/practica2/ejer16.cs
@@ -0,0 +1,6 @@
+/* 16. Escribir una función (método int Fac(int n)) que calcule el factorial de un número n
+pasado al programa como parámetro por la línea de comando
+a) Definiendo una función no recursiva
+b) Definiendo una función recursiva
+c) idem a b) pero con expression-bodied methods (Tip: utilizar el operador condicional
+ternario)*/
