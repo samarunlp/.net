b6940a7a6d70d65a012dc52548940bfa5c65d248
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-30 17:40:28 -0300

    Create ejer13.cs

---
+++ Practica/practica2/ejer13.cs
@@ -0,0 +1,8 @@
+/*
+13. Definir el tipo de datos enumerativo llamado Meses y utilizarlo para:
+a) Imprimir en la consola el nombre de cada uno de los meses en orden inverso (diciembre,
+noviembre, octubre ..., enero)
+c) Solicitar al usuario que ingrese un texto y responder si el texto tipeado corresponde al
+nombre de un mes
+Nota: en todos los casos utilizar un for iterando sobre una variable de tipo Meses
+*/
